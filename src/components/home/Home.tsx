ec360dbf7e88ba1d71774b02d168c46eb435e22b
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-01-18 01:16:28 +0100

    apollo client

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,9 +2,29 @@ import React from "react";
 import Products from "../products/products";
 import PageTitle from "../pageTitle/page_title";
 import useStyles from "./styles";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
 
 const Home = () => {
   const classes = useStyles();
+  const query = gql`
+    {
+      products(first: 5) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `;
+  const res = useQuery(query);
+  console.log(res);
+
+  // console.log(data);
+  // if (loading) return <div>Loading....</div>;
+  // if (error) return <h1>Error! ${error.message}</h1>;
+
   return (
     <div className={classes.prodcuts}>
       <PageTitle
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, createHttpLink, useQuery } from "@apollo/client";
 
+const link = createHttpLink({
+  uri: "https://fourniture-store.myshopify.com/api/2022-01/graphql.json",
+  credentials: "include",
+
+  headers: {
+    "Content-type": "application/graphql",
+    "X-Shopify-Storefront-Access-Token": "08791563014a9dd7a4bcc0b15cc8fce8",
+  },
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
+// fetch("https://fourniture-store.myshopify.com/api/2022-01/graphql.json");
+
+// const client = ...
+// const query = gql`
+//   {
+//     products(first: 5) {
+//       edges {
+//         node {
+//           id
+//         }
+//       }
+//     }
+//   }
+// `;
+
+// const { data } = useQuery(query);
+// console.log(data);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
